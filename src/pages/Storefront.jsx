ecfcc74013ed32d2d0fda6253c311ad8f36abb22
Carrnage <Carrnage4@gmail.com>
2023-06-07 19:28:24 +1200

    AHhhhhh

--- src/pages/Storefront.jsx
+++ src/pages/Storefront.jsx
@@ -2,104 +2,62 @@ import { Grid , Card, Text, Button, Badge } from "@nextui-org/react";
 import { Children, useState } from "react";
 import { Link } from "react-router-dom";
 
-let id = null
-
-
 function Store() {
-    const [Stock, setStock] = useState({
-        pieStock:10,
-        appleStock:3,
-        fishStock:5
-    });
     const [Cart, setCart] = useState({
         pieCart:0,
         appleCart:0,
         fishCart:0
     });
-    console.log(Object.keys(Stock).length+"test1");
+
+    const ItemArray = [
+        {
+            id:0,
+            name: "Pie",
+            cost: "$5.00",
+            stock: 5
+        }, {
+            id:1,
+            name: "Apple",
+            cost: "$50.00",
+            stock: 10
+        }, {
+            id:2,
+            name: "Fish",
+            cost: "$500.00",
+            stock: 3
+    
+        }
+    ]
+    const [Items, setItems] = useState({ItemArray});
+    
+    const handleClick = (k) => {
+        console.log(k.name)
+    }
+
     return (
         <div>
             <Grid.Container>
-                <Grid>
+                {ItemArray.map( ( k) => 
+                <Grid key={k}>
                     <Card>
                         <Card.Body>
                             <Text>
-                              Pie
+                              {k.name}
                             </Text>
                         </Card.Body>
                         <Card.Footer>
-                            <Badge content={Stock.pieStock}>
-                                <Button onClick={(e) => handleClick(e)}>
-                                    Add to Cart
+                            <Badge content={k.stock}>
+                                <Button onPress={() => handleClick(k)}>
+                                    Add to Cart {k.cost}
                                 </Button>
                             </Badge>
                         </Card.Footer>
                     </Card>
                 </Grid>
-                <Grid>
-                    <Card>
-                        <Card.Body>
-                            <Text>
-                              Apple
-                            </Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <Badge content={Stock.appleStock}>
-                                <Button>
-                                    Add to Cart
-                                </Button>
-                            </Badge>
-                        </Card.Footer>
-                    </Card>
-                </Grid>
-                <Grid>
-                    <Card>
-                        <Card.Body>
-                            <Text>
-                              Fish
-                            </Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <Badge content={Stock.fishStock}>
-                                <Button>
-                                    Add to Cart
-                                </Button>
-                            </Badge>
-                        </Card.Footer>
-                    </Card>
-                </Grid>
-
+                )}
             </Grid.Container>
         </div>
     );
-    function handleClick({e}) {
-        console.log(e)
-        switch (e) {
-            case  1:
-            Stock.pieStock++
-            setStock({...Stock})
-            Cart.pieCart++
-            setCart({...Cart})
-            break
-        case 2:
-            Stock.appleStock--
-            setStock({...Stock})
-            Cart.appleCart++
-            setCart({...Cart})
-            break
-        case 3:            
-            Stock.fishStock--
-            setStock({...Stock})
-            Cart.fishCart++
-            setCart({...Cart})
-            break
-        default:
-            console.log("This is outside of use cases",id)
-            break
-        }
-        
-    }
-    
 }
 
 export default Store;
