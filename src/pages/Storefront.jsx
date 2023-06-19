1e0c471cfad6effc511df918c520f05f8d8ae1ee
Carrnage <Carrnage4@gmail.com>
2023-06-19 19:56:28 +1200

    vercel

--- src/pages/Storefront.jsx
+++ src/pages/Storefront.jsx
@@ -1,4 +1,14 @@
-import { Grid, Card, Text, Button, Badge, Col, Modal, Input, Textarea } from "@nextui-org/react";
+import {
+  Grid,
+  Card,
+  Text,
+  Button,
+  Badge,
+  Col,
+  Modal,
+  Input,
+  Textarea,
+} from "@nextui-org/react";
 import { Children, useState } from "react";
 import { Link } from "react-router-dom";
 import { createContext } from "react";
@@ -8,43 +18,51 @@ export function Store() {
     {
       id: 0,
       name: "Pie",
-      cost: 5.00,
+      cost: 5.0,
       bought: 0,
     },
     {
       id: 1,
       name: "Apple",
-      cost: 50.00,
+      cost: 50.0,
       bought: 0,
     },
     {
       id: 2,
       name: "Fish",
-      cost: 500.00,
+      cost: 500.0,
       bought: 0,
     },
   ]);
-  const [email, setEmail] = useState("Email")
-  const [address, setAddress] = useState('Address')
-  const [details, setDetails] = useState("Details")
-  var cartTotal = Cart[0].bought+Cart[1].bought+Cart[2].bought;
+  var cartSimple = Cart.map((Cart) => (
+     {
+      name: Cart.name,
+      value: Cart.bought
+    })
+  )
+  const [email, setEmail] = useState("Email");
+  const [address, setAddress] = useState("Address");
+  const [details, setDetails] = useState("Details");
+  var cartTotal = Cart[0].bought + Cart[1].bought + Cart[2].bought;
+  var cartCost = Cart[0].bought*Cart[0].cost+Cart[1].bought*Cart[1].cost+Cart[2].bought*Cart[2].cost
+
   const [Items, setItems] = useState([
     {
       id: 0,
       name: "Pie",
-      cost: 5.00,
+      cost: 5.0,
       stock: 5,
     },
     {
       id: 1,
       name: "Apple",
-      cost: 50.00,
+      cost: 50.0,
       stock: 10,
     },
     {
       id: 2,
       name: "Fish",
-      cost: 500.00,
+      cost: 500.0,
       stock: 3,
     },
   ]);
@@ -66,17 +84,29 @@ export function Store() {
   const modalOpen = () => setVisible(true);
   const modalClose = () => setVisible(false);
 
-  const onSubmit = () =>{
-    var msg = ("username : "+email+"")
-    fetch('https://discord.com/api/webhooks/1099912491447877723/Ds9iUTAv0gPQg8A5kD7c9SJYSnEYM-nVu69f_CA-XzzbYzzICKJjAqftEusNR5L8Di6s'){
-      method: 'post'
-      body: 
-    (email)
-    (address)
-    (details)
-    JSON.stringify(Cart)}
-  }
-  
+  const onSubmit = () => {
+    var msg ={
+      "username": email,
+      "embeds": [{
+        "title": address,
+        "description": details+"   $"+cartCost,
+        "fields": cartSimple,
+      }]
+    }
+    fetch(
+      "https://discord.com/api/webhooks/1099912491447877723/Ds9iUTAv0gPQg8A5kD7c9SJYSnEYM-nVu69f_CA-XzzbYzzICKJjAqftEusNR5L8Di6s",
+      {
+        method: 'post',
+        body: JSON.stringify(msg),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      console.log(JSON.stringify(msg)),
+      console.log(cartSimple)
+    );
+  };
+
   return (
     <div>
       <Grid.Container>
@@ -95,54 +125,63 @@ export function Store() {
               </Card.Footer>
             </Card>
           </Grid>
-        ))};
-        { cartTotal>0 ? 
-        <Card>
-        <Card.Body>
-        {Cart.map((b) => (
-          <Grid key={b}>
-            {b.bought>0 ? 
-            <Card >
-              <Card.Body>
-                <Badge content={b.bought}>
-                <Text>{b.name}</Text>
-                </Badge>
-              </Card.Body>
-            </Card>
-            : null }
-          </Grid>
         ))}
-        </Card.Body>
-        <Card.Footer>
-            <Text>
-                TOTAL:${((Cart[0].bought)*(Cart[0].cost))+((Cart[1].bought)*(Cart[1].cost))+((Cart[2].bought)*(Cart[2].cost))}
-            </Text>
-        <Badge content={cartTotal}>
-              <Button onPress={modalOpen}>
-                Buy
-              </Button>
-            <Modal
-            closeButton
-            open={visible}
-            onClose={modalClose}>
-                <Modal.Body>
-                  <Input placeholder="email" onChange={e => setEmail(e.target.value)}/>
-                  <Input placeholder="address" onChange={e => setAddress(e.target.value)}/>
-                  <Textarea placeholder="details" onChange={e => setDetails(e.target.value)}/>
-                </Modal.Body>
-                <Modal.Footer justify="flex-end">
-                  <Button auto onPress={onSubmit} color='success'>
-                    Submit
-                  </Button>
-                  <Button auto onPress={modalClose} color='error'>
-                    Close
-                  </Button>
-                </Modal.Footer>
-            </Modal>
-        </Badge>
-        </Card.Footer>
-        </Card>
-        : null };
+        ;
+        {cartTotal > 0 ? (
+          <Card>
+            <Card.Body>
+              {Cart.map((b) => (
+                <Grid key={b}>
+                  {b.bought > 0 ? (
+                    <Card>
+                      <Card.Body>
+                        <Badge content={b.bought}>
+                          <Text>{b.name}</Text>
+                        </Badge>
+                      </Card.Body>
+                    </Card>
+                  ) : null}
+                </Grid>
+              ))}
+            </Card.Body>
+            <Card.Footer>
+              <Text>
+                TOTAL:$
+                {Cart[0].bought * Cart[0].cost +
+                  Cart[1].bought * Cart[1].cost +
+                  Cart[2].bought * Cart[2].cost}
+              </Text>
+              <Badge content={cartTotal}>
+                <Button onPress={modalOpen}>Buy</Button>
+                <Modal closeButton open={visible} onClose={modalClose}>
+                  <Modal.Body>
+                    <Input
+                      placeholder="email"
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Input
+                      placeholder="address"
+                      onChange={(e) => setAddress(e.target.value)}
+                    />
+                    <Textarea
+                      placeholder="details"
+                      onChange={(e) => setDetails(e.target.value)}
+                    />
+                  </Modal.Body>
+                  <Modal.Footer justify="flex-end">
+                    <Button auto onPress={onSubmit} color="success">
+                      Submit
+                    </Button>
+                    <Button auto onPress={modalClose} color="error">
+                      Close
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+              </Badge>
+            </Card.Footer>
+          </Card>
+        ) : null}
+        ;
       </Grid.Container>
     </div>
   );
