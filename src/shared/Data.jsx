196ad27e3f6ed91e341329ec93e4cacd46107039
Carrnage <Carrnage4@gmail.com>
2023-06-19 19:55:07 +1200

    Final

---
+++ src/shared/Data.jsx
@@ -0,0 +1,32 @@
+
+{
+    "username":"Bakery",
+    "content":"Purchase Order",
+    "embeds":[
+        {"title":"Address: Address","url":"Email","description":"Details: Details"}
+    ]}
+
+{
+    "application_id": null,
+     "avatar": null,
+      "channel_id": "1099912325315690588",
+       "guild_id": "693947671269998664",
+        "id": "1099912491447877723",
+         "name": "709-bot",
+          "type": 1,
+           "user": 
+           {"id": "160544275958857729",
+            "username": "trekiewhcs",
+             "avatar": "016a2def339314b3b4483e72ec1072d9",
+              "discriminator": "0",
+               "public_flags": 0,
+                "flags": 0,
+                 "banner": null,
+                  "accent_color": null,
+                   "global_name": "Trekiewhcs",
+                    "avatar_decoration": null,
+                     "display_name": "Trekiewhcs",
+                      "banner_color": null},
+                       "token": "Ds9iUTAv0gPQg8A5kD7c9SJYSnEYM-nVu69f_CA-XzzbYzzICKJjAqftEusNR5L8Di6s",
+                        "url": "https://discord.com/api/webhooks/1099912491447877723/Ds9iUTAv0gPQg8A5kD7c9SJYSnEYM-nVu69f_CA-XzzbYzzICKJjAqftEusNR5L8Di6s"
+                    }
